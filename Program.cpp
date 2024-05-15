68e2b49a6a926196db3eca76c1652a6d0eaa0ed8
Mark Tabatskyi <vasenka688@gmail.com>
2024-05-15 18:15:07 +0300

    changed static array to dynamic

--- Program.cpp
+++ Program.cpp
@@ -6,126 +6,132 @@
 #include <stdlib.h> 
 #include <string.h>
 
+int currentLinesNum = 100;
+int currentLenghNum = 256;
 
-const int MAX_LINES = 100;
-const int MAX_LENGTH = 256;
-
-char memory[MAX_LINES][MAX_LENGTH];
+char** memory;
 int currentLine = 0;
 
-int main()
+void freeMemory(char** memory)
 {
-	start:
-	char command;
-	char inputBuffer[MAX_LENGTH] = { 0 };
-	FILE* file;
-	char filename[100] = "myfile.txt";
-	printf(">");
+    for (int i = 0; i < currentLinesNum; i++) {
+        free(memory[i]);
+    }
+    free(memory);
+}
 
-	(void)scanf(" %c", &command);
+int main() {
+    memory = (char**)malloc(currentLinesNum * sizeof(char*));
+    if (!memory) {
+        printf("Memory allocation failed.\n");
+        return 1;
+    }
+    for (int i = 0; i < currentLinesNum; i++) {
+        memory[i] = (char*)malloc(currentLenghNum * sizeof(char));
+        if (!memory[i]) {
+            printf("Memory allocation failed.\n");
 
-	switch (command)
-	{
-	case 'a':
-		printf(">Enter text to append: ");
-		(void)scanf(" %s", &inputBuffer);
+            freeMemory(memory);
+            return 1;
+        }
+        memory[i][0] = '\0';  
+    }
 
-		if (strlen(memory[currentLine]) + strlen(inputBuffer) < MAX_LENGTH) 
-		{
-			strcat(memory[currentLine], inputBuffer);
-		}
-		else 
-		{
-			printf("Error: Line length exceeds the maximum allowed.\n");
-		}
-			break;
-	case 'n':
-		printf(">New line started\n");
-		if (currentLine < MAX_LINES - 1) 
-		{
-			currentLine++;
-		}
-		else 
-		{
-			printf("Error: Reached maximum number of lines.\n");
-		}
-			break;
-	case 's':
-		printf(">Enter filename for saving: ");
-		(void)scanf(" %s", &filename);
+    char command;
+    char* inputBuffer = (char*)malloc(currentLinesNum * sizeof(char));
+    FILE* file;
+    char filename[100] = "myfile.txt";
 
-		file = fopen(filename, "w");
-		if (file != NULL)
-		{
-			for (int i = 0; i <= currentLine; i++) 
-			{
-				fprintf(file, "%s\n", memory[i]);
-			}
-			fclose(file);
-			printf(">Save succesful\n");
-		}
-		else 
-		{
-			printf(">Error opening file\n");
-		}
-			break;
-	case 'l':
-		printf(">Enter filename for loading: ");
-		(void)scanf(" %s", &filename);
+start:
+    printf(">");
+    (void)scanf(" %c", &command);
 
-		file = fopen(filename, "r");
-		if (file != NULL)
-		{
-			currentLine = 0;
-			while (fgets(inputBuffer, MAX_LENGTH, file) != NULL && currentLine < MAX_LINES)
-			{
-				inputBuffer[strlen(inputBuffer) - 1] = 0;
-				strcpy(memory[currentLine], inputBuffer);
+    switch (command) {
+    case 'a':
+        printf(">Enter text to append: ");
+        (void)scanf(" %[^\n]s", inputBuffer);
 
-				if (currentLine++ >= MAX_LINES)
-				{
-					printf(">Reached maximum capacity\n");
-					break;
-				}
-			}
-			fclose(file);
-			currentLine--;
+        if (strlen(memory[currentLine]) + strlen(inputBuffer) < currentLenghNum) {
+            strcat(memory[currentLine], inputBuffer);
+        }
+        else {
+            printf("Error: Line length exceeds the maximum allowed.\n");
+        }
+        break;
+    case 'n':
+        printf(">New line started\n");
+        if (currentLine < currentLinesNum - 1) {
+            currentLine++;
+        }
+        else {
+            printf("Error: Reached maximum number of lines.\n");
+        }
+        break;
+    case 's':
+        printf(">Enter filename for saving: ");
+        (void)scanf(" %s", filename);
 
-			printf(">Load succesful\n");
-		}
-		else
-		{
-			printf(">Error opening file\n");
-		}
-			break;
-	case 'p':
-		for (int i = 0; i <= currentLine; i++) 
-		{
-			printf("%d: %s\n", i + 1, memory[i]);
-		}
-			break;
-	case 'i':
-		char indexBuffer[3];
-		printf(">Choose line and index: ");
-		(void)scanf(" %s", &indexBuffer);
-		
-		char* split;
-		split = strtok(indexBuffer, " ");
+        file = fopen(filename, "w");
+        if (file != NULL) {
+            for (int i = 0; i <= currentLine; i++) {
+                fprintf(file, "%s\n", memory[i]);
+            }
+            fclose(file);
+            printf(">Save successful\n");
+        }
+        else {
+            printf(">Error opening file\n");
+        }
+        break;
+    case 'l':
+        printf(">Enter filename for loading: ");
+        (void)scanf(" %s", filename);
 
-		while (split != NULL)
-		{
-			printf("%s, ", split);
-			split = strtok(NULL, " ");
-		}
-			break;
-	case 'f':
-		printf(">not implemented yet\n");
-			break;
-	default:
-		printf(">unknown function\n");
-			break;
-	}
-	goto start;
+        file = fopen(filename, "r");
+        if (file != NULL) {
+            currentLine = 0;
+            while (fgets(inputBuffer, currentLenghNum, file) != NULL && currentLine < currentLinesNum) {
+                inputBuffer[strlen(inputBuffer) - 1] = 0;  
+                strcpy(memory[currentLine], inputBuffer);
 
-	return 0;
+                if (currentLine++ >= currentLinesNum) {
+                    printf(">Reached maximum capacity\n");
+                    break;
+                }
+            }
+            fclose(file);
+            currentLine--;
+            printf(">Load successful\n");
+        }
+        else {
+            printf(">Error opening file\n");
+        }
+        break;
+    case 'p':
+        for (int i = 0; i <= currentLine; i++) {
+            printf("%d: %s\n", i + 1, memory[i]);
+        }
+        break;
+    case 'i':
+        int line, index;
+        printf(">Choose line: ");
+        (void)scanf(" %d", &line);
+        printf(">Choose index: ");
+        (void)scanf(" %d", &index);
+
+
+
+        break;
+    case 'f':
+        printf(">not implemented yet\n");
+        break;
+    default:
+        printf(">unknown function\n");
+        break;
+    }
+    goto start;
+
+    freeMemory(memory);
+
+    return 0;
 }
